0112c3ec52dfaadef2798573a239a8159317f2df
Parker Jones <parkerjones1036@gmail.com>
2024-06-22 23:37:01 -0400

    used a FramerMotion animation in Hero section

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,9 +1,24 @@
 import { Container } from "../components/container";
 import { Button } from "../components/button";
+import { motion } from "framer-motion";
 
 const Content = () => {
   return (
-    <div className="flex flex-col items-center justify-center lg:items-start">
+    <motion.div
+      initial={{
+        x: "-100vw",
+      }}
+      animate={{
+        x: 0,
+      }}
+      transition={{
+        type: "tween",
+        stiffness: 20,
+        damping: 20,
+        duration: 1.5,
+      }}
+      className="flex flex-col items-center justify-center lg:items-start"
+    >
       <div className="group mb-8 flex max-w-[300px] items-center justify-center rounded-md bg-[#202C3380] px-[3px] py-[7px] text-[20px] text-white lg:px-[6px] lg:py-[8px]">
         <h1 class="relative overflow-hidden py-2">
           <span class="absolute inset-x-0 bottom-0 h-1 origin-left scale-x-0 bg-white transition-transform duration-200 ease-in-out group-hover:scale-x-100"></span>
@@ -19,7 +34,7 @@ const Content = () => {
         meticulous attention to detail and unwavering passion.
       </p>
       <Button text={"Request a Project"} />
-    </div>
+    </motion.div>
   );
 };
 const Hero = () => {
